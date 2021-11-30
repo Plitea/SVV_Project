b07e179bc8465bac583a086944216b305aac258d
Plitea <dragosplitea@yahoo.com>
2021-11-30 17:30:11 +0200

    adding server states and the possibility of choosing ports

--- src/main/java/com/dragos/HttpServer.java
+++ src/main/java/com/dragos/HttpServer.java
@@ -1,27 +1,62 @@
 package com.dragos;
 
-import java.io.*;
-import java.net.*;
-import java.nio.file.FileVisitResult;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.Files;
 import java.util.StringTokenizer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URLDecoder;
 
 
-public class HttpServer extends Thread {
+public class HttpServer implements Runnable {
 
     protected Socket cs;    //client socket
+    static ServerSocket ss;   //server socket
 
-    private static final File webroot = new File("./HTML_files");
-    private static final String default_page = "home.html";
-    private static final String FILE_NOT_FOUND= "404_not_found.html";
-    static final int PORT = 8080;
+    private static final File WEB_ROOT = new File("C:\\Users\\drago\\IdeaProjects\\myhttpserver\\HTML_files");
+    private static final String DEFAULT_FILE = "index.html";
+    private static final String FILE_NOT_FOUND = "404_not_found.html";
+    private static final String FILE_MAINTENANCE = "maintenance.html";
+    private static final String METHOD_NOT_SUPPORTED = "not_supported.html";
 
-    private static String foundPath = "";
+    static int serverPort = 0;
+    public int conection;
+    static int status = 0;
+
+    public int getStateServer() {
+        return status;
+    }
+
+    public void setClientSocket(Socket clientSocket) {
+        this.cs = clientSocket;
+    }
+
+    public boolean setPort(int portNr) {
+        try (ServerSocket ignored = new ServerSocket(portNr)) {
+            serverPort = portNr;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void setState(int state) {
+        status = state;
+    }
+
+    public void setConnectionOK(int x) {
+        this.conection = x;
+    }
 
     public String getContentType(String fileReq) {
 
@@ -30,12 +65,11 @@ public class HttpServer extends Thread {
         if(fileReq.endsWith(".css"))
             return "other/css";
         return "text/plain";
-
     }
 
     private void fileNotFound(PrintWriter printWriter, OutputStream dataOut) throws IOException {
 
-        File file = new File(webroot, FILE_NOT_FOUND);
+        File file = new File(WEB_ROOT, FILE_NOT_FOUND);
         int fileLength = (int) file.length();
         String content = "text/html";
         byte[] fileData = readFileData(file, fileLength);
@@ -53,123 +87,181 @@ public class HttpServer extends Thread {
 
     }
 
-    public String parseSpaces(String fileReq) {
+    public byte[] readFileData(File file,int fileLength) throws IOException {
 
-        if (fileReq.contains("%20")) {
-
-            fileReq = fileReq.replace("%20", " ");
-        }
-
-        return fileReq;
-    }
-
-
-    private static void searchFileTree(String fileReq) throws IOException {
-
-        Path root = Paths.get(webroot.toString());
-
-        Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
-
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (file.toString().contains(fileReq))
-                    foundPath = file.toString();
-                return FileVisitResult.CONTINUE;
-            }
-        }
-        );
-    }
-
-    public byte[] readFileData(File file, int fileLength) throws IOException {
-
-        System.out.println("File: " + file);
         FileInputStream fileIn = null;
         byte[] fileData = new byte[fileLength];
-
-        try {
+        try{
 
             fileIn = new FileInputStream(file);
             fileIn.read(fileData);
-
-        } finally {
+        }finally{
 
             if (fileIn != null)
                 fileIn.close();
-
         }
-
         return fileData;
-
     }
 
-    public HttpServer(Socket clientSocet) {
 
-        cs = clientSocet;
-        start();
+    public boolean acceptServerPort() {
+        try{
 
+            if(serverPort < 8000 || serverPort > 10500) {
+                throw new Exception();
+            }else {
+
+                ss = new ServerSocket(serverPort);
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+
+    public void listenClients(){
+
+        while(true){
+
+            HttpServer connection;
+            try {
+
+                connection = new HttpServer();
+                connection.setClientSocket(ss.accept());
+                setConnectionOK(1);
+                Thread thread = new Thread(connection);
+                thread.start();
+            }catch (IOException e) {
+
+                setConnectionOK(0);
+            }
+        }
+    }
+
+    @Override
     public void run() {
 
-        System.out.println("New Communication Thread Started");
-        PrintWriter out = null;
         BufferedReader in = null;
+        PrintWriter out = null;
         BufferedOutputStream dataOut = null;
         String fileReq = null;
 
         try {
 
-            out = new PrintWriter(cs.getOutputStream(), true);
-
             in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-
+            out = new PrintWriter(cs.getOutputStream());
             dataOut = new BufferedOutputStream(cs.getOutputStream());
 
-            String inputLine = in.readLine();
+            String input = in.readLine();
+            StringTokenizer parse= new StringTokenizer(input);
+            String method = parse.nextToken().toUpperCase();
 
-            StringTokenizer tokens = new StringTokenizer(inputLine);
-            String method = tokens.nextToken().toUpperCase();
-            fileReq = parseSpaces(tokens.nextToken().toLowerCase());
+            fileReq = URLDecoder.decode(parse.nextToken().toLowerCase(),"UTF-8");  //get file request
 
-            if (method.equals("GET")) { //GET method -> return content
+            if(!method.equals("GET") && !method.equals("HEAD"))
+            {
+                System.out.println("501 Not implemented: " + method);
+                writeFileData(METHOD_NOT_SUPPORTED,out,dataOut,"HTTP/1.1 501 Not Implemented");
 
-                if (fileReq.endsWith("/")) {
-                    fileReq += default_page;
+            }else{
+
+                if(method.equals("GET"))   {             //GET method -> return content
+
+                    if(status == 1){
+
+                        if(fileReq.endsWith("/"))
+                            {
+                                fileReq += DEFAULT_FILE;
+                            }
+
+                    writeFileData(fileReq,out,dataOut,"HTTP/1.1 200 OK");   //return content
+                }else if(status == 2) {
+
+                    fileReq = FILE_MAINTENANCE;
+                    writeFileData(fileReq,out,dataOut,"HTTP/1.1 200 OK");
+
+                }else if(status == 3){
+
+                    // close  server
+                    in.close();
+                    out.close();
+                    dataOut.close();
+                    cs.close();
                 }
-
-                File file = new File(webroot, fileReq);
-                int fileLength = (int) file.length();
-                String content = getContentType(fileReq);
-
-                byte[] fileData = readFileData(file, fileLength);
-
-                out.println("HTTP/1.1 200 OK");
-                out.println("Server: Java HTTP Server");
-                out.println("Date: " + new Date());
-                out.println("Content-type: " + content);
-                out.println("Content-length: " + fileLength);
-                out.println();  //blank line between headers and content
-                out.flush();    //flush character output stream buffer
-
-                dataOut.write(fileData, 0, fileLength);
-                dataOut.flush();
+                }
             }
+        }catch(FileNotFoundException fnfe)
+        {
+            try{
 
-            out.close();
-            in.close();
-            cs.close();     //close the client socket
+                fileNotFound(out,dataOut);
+                System.out.println("NOT FOUND: " + fileReq + " not found!");
+            }catch(IOException ioe){
 
-        } catch (FileNotFoundException fnfe) {
-            try {
-
-                fileNotFound(out, dataOut);
-            } catch (IOException ioe) {
-
-                System.err.println("Error with FILE NOT FOUND exception : " + ioe.getMessage());
+                System.err.println("+ File not found.  "+ioe.getMessage());
             }
-        } catch (IOException e) {
+        }catch(IOException ioe){
 
-            System.err.println("Communication Server Error");
-            System.exit(1);
+            System.err.println("+ Server error : "+ioe);
+        }finally{
+            try{
+
+                in.close();
+                out.close();
+                dataOut.close();
+                cs.close();
+            }catch(Exception e)
+            {
+                System.err.println("+ Error closing stream:"+e.getMessage());
+            }
         }
     }
+
+    public void writeFileData(String nameOfFileReq,PrintWriter hearderOut,OutputStream bodyOut,String headerText)
+    {
+        File file = new File(WEB_ROOT,nameOfFileReq);
+        int fileLength = (int)file.length();
+        String content = getContentType(file.getName());
+
+        String inputString = "";
+        Charset charset = StandardCharsets.US_ASCII;
+        byte[] fileData =  inputString.getBytes(charset);
+        try {
+            fileData = readFileData(file,fileLength);
+        } catch (IOException e) {
+            file = new File(WEB_ROOT,FILE_NOT_FOUND);
+            fileLength = (int)file.length();
+            content = getContentType(file.getName());
+            try {
+                fileData = readFileData(file,fileLength);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            System.out.println("Error reading : " + file);
+        }
+
+        if (hearderOut != null) {         //send Header
+            hearderOut.println(headerText);
+            hearderOut.println("main.Server");
+            hearderOut.println("Date" + new Date());
+            hearderOut.println("Content type" + content);
+            hearderOut.println("Content length" + fileLength);
+
+            hearderOut.println();   //blank line between headers and content
+            hearderOut.flush(); //flush character output stream buffer
+        }
+
+        try {                           //send data
+            bodyOut.write(fileData,0,fileLength);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bodyOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/com/dragos/HttpServerMain.java
+++ src/main/java/com/dragos/HttpServerMain.java
@@ -1,53 +1,96 @@
 package com.dragos;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.InputStreamReader;
 
-public class HttpServerMain extends HttpServer{
-    public HttpServerMain(Socket clientSocet) {
-        super(clientSocet);
-    }
 
-    public static void main(String[] args){
-        ServerSocket ss = null;
+public class HttpServerMain implements Runnable{
+    static int port;
+    static boolean runServer = true;
+    static String  statusServer;
+    static HttpServer server;
+    static boolean connectionOk = false;
+
+
+
+    public static void main(String[] args) {
+        server = new HttpServer();
+        while (!connectionOk) {
+            choosePort();
+        }
+
+        Thread thread = new Thread(new HttpServerMain());
+        thread.start();
 
         try {
-
-            ss = new ServerSocket(PORT);
-            System.out.println("Socket connection created.");
-
-            try {
-                while (true) {
-
-                    System.out.println("Waiting for connection on port: " + PORT);
-                    new HttpServer(ss.accept()); //stuck here until recieving a connection
-
-                }
-
-            } catch (IOException e) {
-
-                System.err.println("Server failed to accept connection.");
-                System.exit(1);
-
-            }
-
+            readState();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            System.err.println("Error listeing on port on port: " + PORT);
-            System.exit(1);
+    }
 
-        } finally {
+    public static void readState() throws IOException {
+        do {
+            System.out.println("Select server state:  [1] -> RUNNING | [2] -> MAINTENANCE | [3] -> STOPPED");
+            BufferedReader readerCommand = new BufferedReader(new InputStreamReader(System.in));
+            String commandLine = readerCommand.readLine();
+            verifyReadedState(commandLine);
+        } while (true);
+    }
 
-            try {
+    public static void verifyReadedState(String command) {
+        switch (command) {
+            case "1":
 
-                assert ss != null;
-                ss.close();   //closing the server socket connection if it's null
-            } catch (IOException e) {
-                System.err.println("Could not close port: " + PORT);
-                System.exit(1);
+                server.setState(1);
+                statusServer = "RUNNING";
+                System.out.println("Connection created on port:" + port);
+                System.out.println("Server state: " + statusServer);
+                break;
+            case "2":
 
+                server.setState(2);
+                statusServer = "MAINTENANCE";
+                System.out.println("Server state: " + statusServer);
+                break;
+            case "3":
+
+                server.setState(3);
+                statusServer = "STOPPED";
+                System.out.println("Server state: " + statusServer);
+                break;
+        }
+    }
+
+    public static void choosePort() {
+
+        System.out.print("Select a port from [8000:10500] -> ");
+        BufferedReader readerCommand =  new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            port = Integer.parseInt(readerCommand.readLine());
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Invalid port.");
+            port =- 1;
+        }
+
+        if(server.setPort(port))
+        {
+            if(server.acceptServerPort())
+            {
+                System.out.println("The server started.");
+                connectionOk = true;
+                statusServer = "STOPPED";
             }
+        }
+    }
+
+    @Override
+    public void run() {
+        while(runServer) {
+            server.listenClients();
         }
     }
 }
